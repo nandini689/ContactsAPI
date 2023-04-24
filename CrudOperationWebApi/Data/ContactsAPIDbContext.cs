f12fd0052744b75dc9362472e2429b150d14a453
Nandini Gupta <65002182+nandini689@users.noreply.github.com>
2023-04-24 13:38:14 +0530

    ContctsAPI

---
+++ CrudOperationWebApi/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudOperationWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperationWebApi.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
